9249fc8c49810babb0194687eeb2d06d69d1f919
João <joao.antonio2452@outlook.com>
2025-03-03 09:54:54 -0300

    Projeto final

---
+++ ControleDeFluxo/src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(){
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
+
+public class Contador {
+    public static void main(String [] args){
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = ler.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = ler.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        }
+
+        catch(ParametrosInvalidosException e){
+            System.out.print(e.getMessage());
+        }
+        
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm < parametroDois){
+            int contagem = parametroDois - parametroUm;
+            for(int i=0; i <= contagem; i++){
+                System.out.println(parametroUm + i);
+            }
+        }
+        else{
+            throw new ParametrosInvalidosException();
+        }
+
+    }
+
+}
